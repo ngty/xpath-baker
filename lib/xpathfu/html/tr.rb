2f635a9782d18c91ae04acb74b73c256a66abccb
NgTzeYang <ngty77@gmail.com>
2010-05-31 10:28:31 +0800

    Completed spec for tr() (acepting :cells as a hash).

--- lib/xpathfu/html/tr.rb
+++ lib/xpathfu/html/tr.rb
@@ -3,16 +3,19 @@ module XPathFu
     module Tr
 
       def tr(*args)
-        @scope, @config, @match_attrs = parse_args(*args)
-        %\#{@scope}tr[#{tr_cells_conditions(@match_attrs.delete(:cells)).join('][')}]\
+        match_attrs = parse_args(*args)
+        conditions = [
+          tr_cells_conditions(match_attrs.delete(:cells)).join('][')
+        ].compact
+        %\#{scope}tr[#{conditions.join('][')}]\
       end
 
       def tr_cells_conditions(cells)
         case cells
         when Hash
           cells.map do |field, val|
-            header = %\./ancestor::table[1]//th[normalize-space(.)="#{field}"][1]\
-            %\.//td[count(#{header}/preceding-sibling::th)+1][#{header}][normalize-space(.)="#{val}"]\
+            th = %\./ancestor::table[1]//th[#{c(n(t))}=#{c(q(field))}][1]\
+            %\.//td[count(#{th}/preceding-sibling::th)+1][#{th}][#{c(n(t))}=#{c(q(val))}]\
           end
         else
           []
--- spec/html/tr_spec.rb
+++ spec/html/tr_spec.rb
@@ -4,24 +4,53 @@ require 'xpathfu/html'
 module HtmlTrSpecHelpers
 
   def contents_for(path)
-    Nokogiri::HTML(%\
+    (@hdoc = Nokogiri::HTML(%\
       <html>
         <body>
           <table>
-            <tr><th><span>#</span></th><th><span>Full <br/>Name</span></th><th>Gender</th></tr>
+            <tr><th>#</th><th>Full <br/>Name</th><th>Gender</th></tr>
             <tr><td> 1</td><td>Jane <br/>Lee</td><td>Female</td></tr>
             <tr><td>2 </td><td>John <br/>Tan</td><td>Male</td></tr>
+            <tr><td> 3 </td><td>Jim <br/>Ma</td><td>Male</td></tr>
           </table>
         </body>
       </html>
-    \).xpath(path).map(&:text)
+    \)).xpath(path).map(&:text)
   end
 
   def case_sensitive_and_normalized_space_and_full_inner_text_xpath_for(scope, cells)
     "#{scope}tr[%s]" % cells.map do |field, val|
-      header = %\./ancestor::table[1]//th[normalize-space(.)="#{field}"][1]\
-      %\.//td[count(#{header}/preceding-sibling::th)+1][#{header}][normalize-space(.)="#{val}"]\
+      th = %\./ancestor::table[1]//th[normalize-space(.)="#{field}"][1]\
+      %\.//td[count(#{th}/preceding-sibling::th)+1][#{th}][normalize-space(.)="#{val}"]\
     end.join('][')
+  end
+
+  def case_sensitive_and_unnormalized_space_and_full_inner_text_xpath_for(scope, cells)
+    "#{scope}tr[%s]" % cells.map do |field, val|
+      th = %\./ancestor::table[1]//th[.="#{field}"][1]\
+      %\.//td[count(#{th}/preceding-sibling::th)+1][#{th}][.="#{val}"]\
+    end.join('][')
+  end
+
+  def case_sensitive_and_normalized_space_and_direct_text_xpath_for(scope, cells)
+    "#{scope}tr[%s]" % cells.map do |field, val|
+      th = %\./ancestor::table[1]//th[normalize-space(text())="#{field}"][1]\
+      %\.//td[count(#{th}/preceding-sibling::th)+1][#{th}][normalize-space(text())="#{val}"]\
+    end.join('][')
+  end
+
+  def case_insensitive_and_normalized_space_and_full_inner_text_xpath_for(scope, cells)
+    upper_chars, lower_chars = ['A'..'Z', 'a'..'z'].map {|r| r.to_a.join('') }
+    translate = lambda {|s| %\translate(#{s},"#{upper_chars}","#{lower_chars}")\ }
+    "#{scope}tr[%s]" % cells.map do |field, val|
+      th = %\./ancestor::table[1]//th[#{translate['normalize-space(.)']}=#{translate[%\"#{field}"\]}][1]\
+      %\.//td[count(#{th}/preceding-sibling::th)+1][#{th}]\ +
+        %\[#{translate['normalize-space(.)']}=#{translate[%\"#{val}"\]}]\
+    end.join('][')
+  end
+
+  def scoped_args(scope, args)
+    scope ? args.unshift(scope) : args
   end
 
 end
@@ -38,18 +67,18 @@ describe "XPathFu::HTML <tr/> support" do
     end
   end
 
-  shared 'default scoping for {:cells => {header1 => col1, ...}}' do
+  shared 'scoping for {:cells => {header1 => col1, ...}}' do
     extend HtmlTrSpecHelpers
-    should "return path scoped '//' w matching char casing, full inner-text & with space normalized" do
+    should "return scoped path w matching chars casing, full inner-text & with space normalized" do
       cells = {'#' => '2', 'Full Name' => 'John Tan', 'Gender' => 'Male'}
       XPathFu.tr(*@args[cells]).should.equal \
-        case_sensitive_and_normalized_space_and_full_inner_text_xpath_for('//', cells)
+        case_sensitive_and_normalized_space_and_full_inner_text_xpath_for(@scope || '//', cells)
     end
   end
 
-  shared 'default matching for {:cells => {header1 => col1, ...}}' do
+  shared 'matching for {:cells => {header1 => col1, ...}}' do
     extend HtmlTrSpecHelpers
-    should "return path that matches nodes w matching char casing & full inner-text & space normalized" do
+    should "return path that matches nodes w matching chars casing & full inner-text & space normalized" do
       cells = {'#' => '2', 'Full Name' => 'John Tan', 'Gender' => 'Male'}
       contents_for(XPathFu.tr(*@args[cells])).should.equal ['2 John TanMale']
     end
@@ -57,7 +86,7 @@ describe "XPathFu::HTML <tr/> support" do
       cells = {'#' => '2 ', 'Full Name' => 'john tan', 'Gender' => 'male'}
       contents_for(XPathFu.tr(*@args[cells])).should.be.empty
     end
-    should "return path that does not match nodes wo matching char casing" do
+    should "return path that does not match nodes wo matching chars casing" do
       cells = {'#' => '2', 'Full Name' => 'john tan', 'Gender' => 'male'}
       contents_for(XPathFu.tr(*@args[cells])).should.be.empty
     end
@@ -67,72 +96,100 @@ describe "XPathFu::HTML <tr/> support" do
     end
   end
 
-  describe '> default scope for {:cells => {header1 => col1, ...}}' do
-    before { @args = lambda {|cells| [{:cells => cells}] } }
-    behaves_like 'default scoping for {:cells => {header1 => col1, ...}}'
-    behaves_like 'default matching for {:cells => {header1 => col1, ...}}'
-  end
+  {'default' => nil, 'custom valid' => '//table/'}.each do |mode, scope|
 
-  describe '> default scope for {:cells => {header1 => col1, ...}}, {:case_sensitive => true}' do
-    before { @args = lambda {|cells| [{:cells => cells}, {:case_sensitive => true}] } }
-    behaves_like 'default scoping for {:cells => {header1 => col1, ...}}'
-    behaves_like 'default matching for {:cells => {header1 => col1, ...}}'
-  end
-
-  describe '> default scope for {:cells => {header1 => col1, ...}}, {:include_inner_text => true}' do
-    before { @args = lambda {|cells| [{:cells => cells}, {:include_inner_text => true}] } }
-    behaves_like 'default scoping for {:cells => {header1 => col1, ...}}'
-    behaves_like 'default matching for {:cells => {header1 => col1, ...}}'
-  end
-
-  describe '> default scope for {:cells => {header1 => col1, ...}}, {:normalize_space => true}' do
-    before { @args = lambda {|cells| [{:cells => cells}, {:normalize_space => true}] } }
-    behaves_like 'default scoping for {:cells => {header1 => col1, ...}}'
-    behaves_like 'default matching for {:cells => {header1 => col1, ...}}'
-  end
-
-  shared "custom valid scoping {:cells => {header1 => col1, ...}}" do
-    extend HtmlTrSpecHelpers
-    should "return path prefixed '//table/' w matching char casing, full inner-text & with space normalized" do
-      cells = {'#' => '2', 'Full Name' => 'John Tan', 'Gender' => 'Male'}
-      XPathFu.tr(*@args[cells]).should.equal \
-        case_sensitive_and_normalized_space_and_full_inner_text_xpath_for('//table/', cells)
+    describe "> #{mode} scoping for {:cells => {header1 => col1, ...}}" do
+      before do
+        @scope = scope
+        @args = lambda {|cells| scoped_args(scope, [{:cells => cells}]) }
+      end
+      behaves_like "scoping for {:cells => {header1 => col1, ...}}"
+      behaves_like "matching for {:cells => {header1 => col1, ...}}"
     end
-  end
 
-  describe "> custom valid scoping for {:cells => {header1 => col1, ...}" do
-    before { @args = lambda {|cells| ['//table/', {:cells => cells}] } }
-    behaves_like "custom valid scoping {:cells => {header1 => col1, ...}}"
-    behaves_like 'default matching for {:cells => {header1 => col1, ...}}'
-  end
+    describe "> #{mode} scoping for {:cells => {header1 => col1, ...}} & {:case_sensitive => true}" do
+      before do
+        @scope = scope
+        @args = lambda {|cells| scoped_args(@scope, [{:cells => cells}, {:case_sensitive => true}]) }
+      end
+      behaves_like "scoping for {:cells => {header1 => col1, ...}}"
+      behaves_like "matching for {:cells => {header1 => col1, ...}}"
+    end
 
-  describe '> custom valid scoping for {:cells => {header1 => col1, ...}}, {:case_sensitive => true}' do
-    before { @args = lambda {|cells| ['//table/', {:cells => cells}, {:case_sensitive => true}] } }
-    behaves_like 'custom valid scoping {:cells => {header1 => col1, ...}}'
-    behaves_like 'default matching for {:cells => {header1 => col1, ...}}'
-  end
+    describe "> #{mode} scoping for {:cells => {header1 => col1, ...}} & {:include_inner_text => true}" do
+      before do
+        @scope = scope
+        @args = lambda {|cells| scoped_args(@scope, [{:cells => cells}, {:include_inner_text => true}]) }
+      end
+      behaves_like "scoping for {:cells => {header1 => col1, ...}}"
+      behaves_like "matching for {:cells => {header1 => col1, ...}}"
+    end
 
-  describe '> custom valid scoping for {:cells => {header1 => col1, ...}}, {:include_inner_text => true}' do
-    before { @args = lambda {|cells| ['//table/', {:cells => cells}, {:include_inner_text => true}] } }
-    behaves_like 'custom valid scoping {:cells => {header1 => col1, ...}}'
-    behaves_like 'default matching for {:cells => {header1 => col1, ...}}'
-  end
+    describe "> #{mode} scoping for {:cells => {header1 => col1, ...}} & {:normalize_space => true}" do
+      before do
+        @scope = scope
+        @args = lambda {|cells| scoped_args(@scope, [{:cells => cells}, {:normalize_space => true}]) }
+      end
+      behaves_like "scoping for {:cells => {header1 => col1, ...}}"
+      behaves_like "matching for {:cells => {header1 => col1, ...}}"
+    end
 
-  describe '> custom valid scoping for {:cells => {header1 => col1, ...}}, {:normalize_space => true}' do
-    before { @args = lambda {|cells| ['//table/', {:cells => cells}, {:normalize_space => true}] } }
-    behaves_like 'custom valid scoping {:cells => {header1 => col1, ...}}'
-    behaves_like 'default matching for {:cells => {header1 => col1, ...}}'
+    describe "> #{mode} scoping for {:cells => {header1 => col1, ...}} & {:case_sensitive => false}" do
+      extend HtmlTrSpecHelpers
+      before do
+        @scope, @cells = scope, {'#' => '2', 'full name' => 'john tan', 'gender' => 'male'}
+        @args = lambda { scoped_args(@scope, [{:cells => @cells}, {:case_sensitive => false}]) }
+      end
+      should "return scoped path w matching chars (ignore casing), full inner-text & with space normalized" do
+        XPathFu.tr(*@args[]).should.equal \
+          case_insensitive_and_normalized_space_and_full_inner_text_xpath_for(@scope || '//', @cells)
+      end
+      should "return path that matches nodes w matching chars (ignore casing)" do
+        contents_for(XPathFu.tr(*@args[])).should.equal ['2 John TanMale']
+      end
+    end
+
+    describe "> #{mode} scoping for {:cells => {header1 => col1, ...}} & {:normalize_space => false}" do
+      extend HtmlTrSpecHelpers
+      before do
+        @scope, @cells = scope, {'#' => '2 ', 'Full Name' => 'John Tan', 'Gender' => 'Male'}
+        @args = lambda { scoped_args(@scope, [{:cells => @cells}, {:normalize_space => false}]) }
+      end
+      should "return scoped path w matching chars casing, full inner-text & with space unnormalized" do
+        XPathFu.tr(*@args[]).should.equal \
+          case_sensitive_and_unnormalized_space_and_full_inner_text_xpath_for(@scope || '//', @cells)
+      end
+      should "return path that matches nodes w space unnormalized" do
+        contents_for(XPathFu.tr(*@args[])).should.equal ['2 John TanMale']
+      end
+    end
+
+    describe "> #{mode} scoping for {:cells => {header1 => col1, ...}} & {:include_inner_text => false}" do
+      extend HtmlTrSpecHelpers
+      before do
+        @scope, @cells = scope, {'#' => '2', 'Full' => 'John', 'Gender' => 'Male'}
+        @args = lambda { scoped_args(@scope, [{:cells => @cells}, {:include_inner_text => false}]) }
+      end
+      should "return scoped path w matching chars casing, direct text only & with space normalized" do
+        XPathFu.tr(*@args[]).should.equal \
+          case_sensitive_and_normalized_space_and_direct_text_xpath_for(@scope || '//', @cells)
+      end
+      should "return path that matches nodes w matching direct text only" do
+        contents_for(XPathFu.tr(*@args[])).should.equal ['2 John TanMale']
+      end
+    end
+
   end
 
   describe "> custom invalid scoping for {:cells => {header1 => col1, ...}" do
     extend HtmlTrSpecHelpers
-    before { @args = lambda {|cells| ['/table/', {:cells => cells}] } }
-    should "return path prefixed '/table/' w matching char casing, full inner-text & with space normalized" do
+    before { @args = lambda {|cells| ['//xable/', {:cells => cells}] } }
+    should "return path prefixed '//xable/'" do
       cells = {'#' => '2', 'Full Name' => 'John Tan', 'Gender' => 'Male'}
       XPathFu.tr(*@args[cells]).should.equal \
-        case_sensitive_and_normalized_space_and_full_inner_text_xpath_for('/table/', cells)
+        case_sensitive_and_normalized_space_and_full_inner_text_xpath_for('//xable/', cells)
     end
-    should "return path that does not match nodes w matching char casing & full inner-text & space normalized" do
+    should "return path that does not match nodes" do
       cells = {'#' => '2', 'Full Name' => 'John Tan', 'Gender' => 'Male'}
       contents_for(XPathFu.tr(*@args[cells])).should.be.empty
     end
