a851f4702f600922ea46420c3cf6192af925147f
NgTzeYang <ngty77@gmail.com>
2010-06-01 16:02:33 +0800

    Helpers should be as private as possible.

--- lib/xpathfu/html/tr.rb
+++ lib/xpathfu/html/tr.rb
@@ -11,31 +11,33 @@ module XPathFu
         end
       end
 
-      def tr_cells_conditions(cells)
-        case cells
-        when Hash then tr_cells_hash_conditions(cells)
-        when Array then tr_cells_array_conditions(cells)
-        when nil then nil
-        else raise InvalidArgumentError.new('Match attribute :cells must be a Hash or Array.')
+      private
+
+        def tr_cells_conditions(cells)
+          case cells
+          when Hash then tr_cells_hash_conditions(cells)
+          when Array then tr_cells_array_conditions(cells)
+          when nil then nil
+          else raise InvalidArgumentError.new('Match attribute :cells must be a Hash or Array.')
+          end
         end
-      end
 
-      def tr_cells_hash_conditions(cells)
-        cells.empty? ? nil : cells.map do |field, val|
-          th = %\./ancestor::table[1]//th[#{c(n(t))}=#{c(q(field))}][1]\
-          %\./td[count(#{th}/preceding-sibling::th)+1][#{th}][#{c(n(t))}=#{c(q(val))}]\
-        end.join('][')
-      end
+        def tr_cells_hash_conditions(cells)
+          cells.empty? ? nil : cells.map do |field, val|
+            th = %\./ancestor::table[1]//th[#{c(n(t))}=#{c(q(field))}][1]\
+            %\./td[count(#{th}/preceding-sibling::th)+1][#{th}][#{c(n(t))}=#{c(q(val))}]\
+          end.join('][')
+        end
 
-      def tr_cells_array_conditions(cells)
-        cells.empty? ? nil : (
-            if config.match_ordering
-              './td' + cells.map {|val| %\[#{c(n(t))}=#{c(q(val))}]\ }.join('/following-sibling::td')
-            else
-              cells.map {|val| %\./td[#{c(n(t))}=#{c(q(val))}]\ }.join('][')
-            end
-        )
-      end
+        def tr_cells_array_conditions(cells)
+          cells.empty? ? nil : (
+              if config.match_ordering
+                './td' + cells.map {|val| %\[#{c(n(t))}=#{c(q(val))}]\ }.join('/following-sibling::td')
+              else
+                cells.map {|val| %\./td[#{c(n(t))}=#{c(q(val))}]\ }.join('][')
+              end
+          )
+        end
 
     end
   end
