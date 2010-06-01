749b5aed0f2c8de462744c469fcaf4e42c6939a0
NgTzeYang <ngty77@gmail.com>
2010-06-01 09:16:11 +0800

    Removed XPathFu::HTML::Table since we dont need it yet.

--- lib/xpathfu/html.rb
+++ lib/xpathfu/html.rb
@@ -1,12 +1,10 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), '..', 'xpathfu')
-require 'xpathfu/html/table'
 require 'xpathfu/html/tr'
 
 module XPathFu
 
   declare_mode_as :html
 
-  extend HTML::Table
   extend HTML::Tr
 
 end
