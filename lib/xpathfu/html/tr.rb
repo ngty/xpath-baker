6f60170c13c98baabc7e337e30c92dd585186fce
NgTzeYang <ngty77@gmail.com>
2010-06-01 09:13:33 +0800

    Tidying up & use XPathFu::PathBuilding::build to build the final xpath.

--- lib/xpathfu/html/tr.rb
+++ lib/xpathfu/html/tr.rb
@@ -3,28 +3,32 @@ module XPathFu
     module Tr
 
       def tr(*args)
-        match_attrs = parse_args(*args)
-        conditions = [
-          tr_cells_conditions(match_attrs.delete(:cells))
-        ].compact
-        "#{scope}tr%s" % (conditions.empty? ? '' : "[#{conditions.join('][')}]")
+        build(:tr, *args) do |match_attrs|
+          [
+            tr_cells_conditions(match_attrs.delete(:cells)),
+          ].compact
+        end
       end
 
       def tr_cells_conditions(cells)
-        conditions =
-          case cells
-          when Hash
-            cells.map do |field, val|
-              th = %\./ancestor::table[1]//th[#{c(n(t))}=#{c(q(field))}][1]\
-              %\.//td[count(#{th}/preceding-sibling::th)+1][#{th}][#{c(n(t))}=#{c(q(val))}]\
-            end.join('][')
-          when Array
-            cells.empty? ? '' :
-              './/td' + cells.map {|val| %\[#{c(n(t))}=#{c(q(val))}]\ }.join('/following-sibling::td')
-          else
-            raise InvalidArgumentError.new('Match attribute :cells must be a Hash or Array.')
-          end
-        conditions.empty? ? nil : conditions
+        case cells
+        when Hash then tr_cells_hash_conditions(cells)
+        when Array then tr_cells_array_conditions(cells)
+        when nil then nil
+        else raise InvalidArgumentError.new('Match attribute :cells must be a Hash or Array.')
+        end
+      end
+
+      def tr_cells_hash_conditions(cells)
+        cells.empty? ? nil : cells.map do |field, val|
+          th = %\./ancestor::table[1]//th[#{c(n(t))}=#{c(q(field))}][1]\
+          %\.//td[count(#{th}/preceding-sibling::th)+1][#{th}][#{c(n(t))}=#{c(q(val))}]\
+        end.join('][')
+      end
+
+      def tr_cells_array_conditions(cells)
+        cells.empty? ? nil :
+          './/td' + cells.map {|val| %\[#{c(n(t))}=#{c(q(val))}]\ }.join('/following-sibling::td')
       end
 
     end
