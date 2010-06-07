7473b780b79373f3cd059771909192b9e9805205
NgTzeYang <ngty77@gmail.com>
2010-06-07 10:14:29 +0800

    WIP revising available settings, plus added checking of assigned configuration value, & adding rdoc.

--- lib/xpf/configuration.rb
+++ lib/xpf/configuration.rb
@@ -1,27 +1,184 @@
 module XPF
 
   class ConfigSettingNotSupportedError < Exception ; end
+  class InvalidConfigSettingValueError < Exception ; end
 
-  settings = {
-    :case_sensitive => true,
-    :match_ordering => true,
-    :include_inner_text => true,
-    :normalize_space => true,
-    :position => nil
-  }
+  ###
+  # Generated xpaths are affected by the following configurable settings:
+  #
+  # 1 *text_match* affects how text node matching is done:
+  #
+  #   <DIV>
+  #     John <SPAN>Stone <B>Jr</B></SPAN>
+  #   </DIV>
+  #
+  # Using DIV as the reference node:
+  # <table>
+  # <tr>
+  # <th>Modes Supported</th>
+  # <th>Nodes Considered</th>
+  # <th>Texts Matched</th>
+  # <th>XPath Fragment</th>
+  # </tr>
+  # <tr>
+  # <td>:any_text</td>
+  # <td>DIV, SPAN & B</td>
+  # <td>'John ', 'Stone ' & 'Jr'</td>
+  # <td>?DIV[./descendant-or-self::*[text()=?]]</td>
+  # </tr>
+  # <tr>
+  # <td>:descendant_text</td>
+  # <td>SPAN & B</td>
+  # <td>'Stone ' & 'Jr'</td>
+  # <td>?DIV[./descendant::*[text()=?]]</td>
+  # </tr>
+  # <tr>
+  # <td>:self_text</td>
+  # <td>DIV</td>
+  # <td>'John '</td>
+  # <td>?DIV[text()=?]</td>
+  # </tr>
+  # <tr>
+  # <td>:any_inner_text</td>
+  # <td>DIV, SPAN & B</td>
+  # <td>'John Stone Jr', 'Stone Jr', 'Jr'</td>
+  # <td>?DIV[./descendant-or-self::*[.=?]]</td>
+  # </tr>
+  # <tr>
+  # <td>:descendant_inner_text</td>
+  # <td>SPAN & B</td>
+  # <td>'Stone Jr' & 'Jr'</td>
+  # <td>?DIV[./descendant::*[.=?]]</td>
+  # </tr>
+  # <tr>
+  # <td>:self_inner_text</td>
+  # <td>DIV</td>
+  # <td>'John Stone Jr'</td>
+  # <td>?DIV[.=?]</td>
+  # </tr>
+  # </table>
+  #
+  # 2 *case_sensitive* determines if match should be case sensitive:
+  #
+  #   <INPUT VALUE="sunny"/>
+  #
+  # <table>
+  # <tr>
+  # <th>Flags Supported</th>
+  # <th>Texts Matched</th>
+  # <th>XPath Fragment</th>
+  # </tr>
+  # <tr>
+  # <td>true</td>
+  # <td>'sunny'</td>
+  # <td>?INPUT[@VALUE="sunny"]</td>
+  # </tr>
+  # <tr>
+  # <td>false</td>
+  # <td>'sunny', 'SUNNY', 'SuNny', etc</td>
+  # <td>?INPUT[translate(@VALUE,"ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz")="sunny"]</td>
+  # </tr>
+  # <tr>
+  #
+  # 3 *match_ordering*
+  #
+  # 4 *normalize_space*
+  #
+  # 5 *position*
+  #
+  #
+  class Configuration
 
-  keys = settings.keys.map(&:to_s).sort.map(&:to_sym)
-  Configuration = Struct.new(*keys).new(*keys.map {|k| settings[k] })
+    DEFAULT_SETTINGS = {
+      :case_sensitive     => true,
+      :match_ordering     => true,
+      :normalize_space    => true,
+      :include_inner_text => true,
+      :position           => nil,
+      :axis               => :self
+    }
 
-  class << Configuration
-    def merge(settings)
-      config = self.dup
-      settings.each do |setting, val|
-        config.send(:"#{setting}=", val) rescue \
-          raise ConfigSettingNotSupportedError.new("Config setting :#{setting} is not supported !!")
+    SETTING_VALIDATORS = {
+      :case_sensitive     => :is_boolean!,
+      :match_ordering     => :is_boolean!,
+      :normalize_space    => :is_boolean!,
+      :include_inner_text => :is_boolean!,
+      :position           => :is_valid_position!,
+      :axis               => :is_valid_axis!
+    }
+
+    class << self
+
+      DEFAULT_SETTINGS.keys.each do |setting|
+
+        attr_accessor setting
+        alias_method :"#{setting}?", setting
+
+        define_method(:"#{setting}=") do |val|
+          send(SETTING_VALIDATORS[setting], setting, val) &&
+            instance_variable_set(:"@#{setting}", val)
+        end
+
       end
-      config
+
+      ###
+      # Reset settings to default, as declared by DEFAULT_SETTINGS.
+      #
+      def reset
+        DEFAULT_SETTINGS.each {|setting, val| send(:"#{setting}=", val) }
+      end
+
+      ###
+      # Creates & returns a Configuration, customized by +settings+ hash.
+      #
+      # Raises:
+      # * ConfigSettingNotSupportedError if any of the setting is not supported
+      # * InvalidConfigSettingValueError if any setting value is invalid
+      #
+      def new(settings)
+        config, error = self.dup, ConfigSettingNotSupportedError
+        settings.each do |setting, val|
+          setter = :"#{setting}="
+          fail_unless("Config setting :#{setting} is not supported !!", error) do
+            config.respond_to?(setter)
+          end
+          config.send(setter, val)
+        end
+        config
+      end
+
+      private
+
+        def is_boolean!(setting, val)
+          fail_unless("Config setting :#{setting} must be boolean true/false !!") do
+            [true, false].include?(val)
+          end
+        end
+
+        def is_valid_position!(setting, val)
+          fail_unless("Config setting :#{setting} must be nil or a non-zero integer !!") do
+            val.nil? or (val.is_a?(Integer) && val.nonzero?)
+          end
+        end
+
+        def is_valid_axis!(setting, val)
+          axes = %w{
+            ancestor ancestor_or_self child descendant descendant_or_self following
+            following_sibling namespace parent preceding preceding_sibling self
+          }.map(&:to_sym)
+          msg = "Config setting :#{setting} must be any of :%s & :%s !!" %
+            [axes[0..-2].map(&:to_s).join(', :'), axes[-1]]
+          fail_unless(msg) { axes.include?(val) }
+        end
+
+        def fail_unless(msg, error = InvalidConfigSettingValueError)
+          yield or raise(error.new(msg))
+        end
+
     end
+
   end
+
+  Configuration.reset
 
 end
--- spec/xpf/configuration_spec.rb
+++ spec/xpf/configuration_spec.rb
@@ -3,111 +3,105 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 describe "XPF::Configuration" do
 
   describe '> default' do
-
-    should 'have :case_sensitive as true' do
-      XPF.configure {|config| config.case_sensitive.should.be.true }
+    {
+      :case_sensitive     => [true, 'true'],
+      :match_ordering     => [true, 'true'],
+      :include_inner_text => [true, 'true'],
+      :normalize_space    => [true, 'true'],
+      :position           => [nil, 'nil'],
+      :axis               => [:self, ':self']
+    }.each do |setting, args|
+      val, display_val = args
+      should "have :#{setting} as #{display_val}" do
+        XPF.configure {|config| config.send(setting).should.equal val }
+      end
     end
-
-    should 'have :match_ordering as true' do
-      XPF.configure {|config| config.match_ordering.should.be.true }
-    end
-
-    should 'have :include_inner_text as true' do
-      XPF.configure {|config| config.include_inner_text.should.be.true }
-    end
-
-    should 'have :normalize_space as true' do
-      XPF.configure {|config| config.normalize_space.should.be.true }
-    end
-
-    should 'have :position as nil' do
-      XPF.configure {|config| config.position.should.be.nil }
-    end
-
   end
 
-  describe '> configuring' do
-
-    should 'be able to change :case_sensitive' do
-      XPF.configure do |config|
-        config.case_sensitive = false
-        config.case_sensitive.should.be.false
-        config.case_sensitive = true
-        config.case_sensitive.should.be.true
+  describe '> configuring (with valid values)' do
+    {
+      :case_sensitive     => [true, false],
+      :match_ordering     => [true, false],
+      :include_inner_text => [true, false],
+      :normalize_space    => [true, false],
+      :position           => [nil, 1, 10],
+      :axis               => %w{
+        ancestor ancestor_or_self child descendant descendant_or_self following
+        following_sibling namespace parent preceding preceding_sibling self
+      }.map(&:to_sym)
+    }.each do |setting, vals|
+      should "be able to change :#{setting}" do
+        XPF.configure do |config|
+          vals.each do |val|
+            config.send(:"#{setting}=", val)
+            config.send(:"#{setting}").should.equal val
+          end
+        end
       end
     end
-
-    should 'be able to change :match_ordering' do
-      XPF.configure do |config|
-        config.match_ordering = false
-        config.match_ordering.should.be.false
-        config.match_ordering = true
-        config.match_ordering.should.be.true
-      end
-    end
-
-    should 'be able to change :include_inner_text' do
-      XPF.configure do |config|
-        config.include_inner_text = false
-        config.include_inner_text.should.be.false
-        config.include_inner_text = true
-        config.include_inner_text.should.be.true
-      end
-    end
-
-    should 'be able to change :normalize_space' do
-      XPF.configure do |config|
-        config.normalize_space = false
-        config.normalize_space.should.be.false
-        config.normalize_space = true
-        config.normalize_space.should.be.true
-      end
-    end
-
-    should 'be able to change :position' do
-      XPF.configure do |config|
-        config.position = 10
-        config.position.should.equal 10
-        config.position = nil
-        config.position.should.be.nil
-      end
-    end
-
   end
 
-  describe '> merging with a settings hash' do
-
-    before do
-      @orig_settings = {
-        :normalize_space => 'aa',
-        :include_inner_text => 'bb',
-        :match_ordering => 'cc',
-        :case_sensitive => 'dd',
-        :position => 'ee'
-      }
-      @orig_settings.each {|setting, val| XPF::Configuration.send(:"#{setting}=", val) }
-      @should_have_equal_settings = lambda do |configuration, expected_hash|
-        expected_hash.each {|setting, val| configuration.send(setting).should == val }
+  describe '> configuring (with invalid values)' do
+    {
+      :case_sensitive     => ['aa', 'boolean true/false'],
+      :match_ordering     => ['aa', 'boolean true/false'],
+      :include_inner_text => ['aa', 'boolean true/false'],
+      :normalize_space    => ['aa', 'boolean true/false'],
+      :position           => ['aa', 0, 'nil or a non-zero integer'],
+      :axis               => [
+        'aa', 'any of :%s & :%s' % [%w{
+          ancestor ancestor_or_self child descendant descendant_or_self following
+          following_sibling namespace parent preceding preceding_sibling
+        }.join(', :'), 'self']
+      ]
+    }.each do |setting, args|
+      vals, msg = args[0..-2], args[-1]
+      should "raise XPF::InvalidConfigSettingValueError when :#{setting} is assigned invalid value" do
+        XPF.configure do |config|
+          full_msg = "Config setting :#{setting} must be %s !!" % msg
+          vals.each do |val|
+            lambda { config.send(:"#{setting}=", val) }.
+              should.raise(XPF::InvalidConfigSettingValueError).
+              message.should.equal(full_msg)
+          end
+        end
       end
     end
-
-    should 'duplicate a copy of itself' do
-      configuration = XPF::Configuration.merge({})
-      @should_have_equal_settings[configuration, @orig_settings]
-      configuration.object_id.should.not.equal XPF::Configuration
-    end
-
-    should 'have hash overrides its settings' do
-      configuration = XPF::Configuration.merge(settings_hash = {:normalize_space => 'ee'})
-      @should_have_equal_settings[configuration, @orig_settings.merge(settings_hash)]
-    end
-
-    should 'raise XPF::ConfigSettingNotSupportedError if unsupported setting is specified' do
-      lambda { XPF::Configuration.merge(:hello => 'ee') }.
-        should.raise(XPF::ConfigSettingNotSupportedError).
-        message.should.equal('Config setting :hello is not supported !!')
-    end
-
   end
+
+#  describe '> merging with a settings hash' do
+#
+#    before do
+#      @orig_settings = {
+#        :normalize_space => 'aa',
+#        :include_inner_text => 'bb',
+#        :match_ordering => 'cc',
+#        :case_sensitive => 'dd',
+#        :position => 'ee'
+#      }
+#      @orig_settings.each {|setting, val| XPF::Configuration.send(:"#{setting}=", val) }
+#      @should_have_equal_settings = lambda do |configuration, expected_hash|
+#        expected_hash.each {|setting, val| configuration.send(setting).should == val }
+#      end
+#    end
+#
+#    should 'duplicate a copy of itself' do
+#      configuration = XPF::Configuration.merge({})
+#      @should_have_equal_settings[configuration, @orig_settings]
+#      configuration.object_id.should.not.equal XPF::Configuration
+#    end
+#
+#    should 'have hash overrides its settings' do
+#      configuration = XPF::Configuration.merge(settings_hash = {:normalize_space => 'ee'})
+#      @should_have_equal_settings[configuration, @orig_settings.merge(settings_hash)]
+#    end
+#
+#    should 'raise XPF::ConfigSettingNotSupportedError if unsupported setting is specified' do
+#      lambda { XPF::Configuration.merge(:hello => 'ee') }.
+#        should.raise(XPF::ConfigSettingNotSupportedError).
+#        message.should.equal('Config setting :hello is not supported !!')
+#    end
+#
+#  end
 
 end
