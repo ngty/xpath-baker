2257668f467d873b4fc983fff7652a72bf19c9ce
NgTzeYang <ngty77@gmail.com>
2010-06-11 15:52:13 +0800

    Use new helper #translate_casing, & also improve its generic support.

--- spec/xpf/matchers/basic_attribute_shared_spec.rb
+++ spec/xpf/matchers/basic_attribute_shared_spec.rb
@@ -3,9 +3,7 @@ shared 'basic attribute matcher' do
   describe '> generating condition (with valid value)' do
 
     before do
-      uc, lc = [('A'..'Z'), ('a'..'z')].map {|r| r.to_a.join('') }
-      @translate = lambda {|s| %|translate(#{s},"#{uc}","#{lc}")| }
-      @name, @val = 'attr-x', 'val-x'
+      @name, @val = @name || 'attr-x', 'val-x'
       @default = %|normalize-space(@#{@name})="#{@val}"|
       @condition_should_equal = lambda do |config, expected|
         @attr_matcher.new(@name, @val, XPF::Configuration.new(config)).
@@ -26,7 +24,7 @@ shared 'basic attribute matcher' do
     end
 
     should 'not be case-sensitive when config[:case_sensitive] is false' do
-      expected = ["normalize-space(@#{@name})", %|"#{@val}"|].map{|s| @translate[s] }.join('=')
+      expected = ["normalize-space(@#{@name})", %|"#{@val}"|].map{|s| translate_casing(s) }.join('=')
       @condition_should_equal[{:case_sensitive => false}, expected]
     end
 
@@ -40,7 +38,7 @@ shared 'basic attribute matcher' do
   describe '> generating condition (with invalid value NIL_VALUE)' do
 
     before do
-      @name, @val = 'attr-x', XPF::Matchers::Matchable::NIL_VALUE
+      @name, @val = @name || 'attr-x', XPF::Matchers::Matchable::NIL_VALUE
       @default = %|normalize-space(@#{@name})|
       @condition_should_equal = lambda do |config, expected|
         XPF::Matchers::Attribute.new(@name, @val, XPF::Configuration.new(config)).
