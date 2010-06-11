b8287a65304ee201a62c3224e0296c2b12cdd30e
NgTzeYang <ngty77@gmail.com>
2010-06-11 16:53:34 +0800

    Completed XPF::HTML::Matchers::Attribute.

--- lib/xpf/html/matchers/attribute.rb
+++ lib/xpf/html/matchers/attribute.rb
@@ -6,13 +6,13 @@ module XPF
         def condition
           !("#{name}".downcase.to_sym == :class && value.is_a?(Array)) ? super : (
             value.map do |val|
-              expr, val = c(n("@#{name}")), val.strip
-              '(%s)' % %W|
-                %s=#{c(q(val))}
-                contains(%s,#{c(q(' %s '))})
-                starts-with(%s,#{c(q('%s '))})
-                ends-with(%s,#{c(q(' %s'))})
-              |.join(' or ') % ([expr,val]*4)
+              tokens = [c(n("@#{name}")), val.strip]*4
+              '(%s)' % [
+                %|%s=#{c(q('%s'))}|,
+                %|contains(%s,#{c(q(' %s '))})|,
+                %|starts-with(%s,#{c(q('%s '))})|,
+                %|ends-with(%s,#{c(q(' %s'))})|
+              ].join(' or ') % tokens
             end.join(' and ')
           )
         end
--- spec/html/matchers/attribute_spec.rb
+++ spec/html/matchers/attribute_spec.rb
@@ -1,10 +1,117 @@
-require File.join(File.dirname(__FILE__), '..', '..', 'spec_helper')
-require File.join(File.dirname(__FILE__), '..', '..', 'xpf', 'matchers', 'basic_attribute_shared_spec')
+require File.join(spec_root = File.join(File.dirname(__FILE__), '..', '..'), 'spec_helper')
+require File.join(spec_root, 'xpf', 'matchers', 'basic_attribute_shared_spec')
 require 'xpf/html'
 
-describe "XPF::HTML::Matchers::Attribute" do
+describe "XPF::HTML::Matchers::Attribute (w.r.t @class)" do
 
-  before { @attr_matcher = XPF::HTML::Matchers::Attribute }
+  before do
+    @attr_matcher = XPF::HTML::Matchers::Attribute
+    @name, @val = :class, 'blue red'
+  end
+
   behaves_like 'basic attribute matcher'
 
+  describe '> generating condition (w @class as a single element array)' do
+
+    before do
+      @name, @val = :class, %w{aweso-me}
+      @default = '(%s)' % [
+        %|normalize-space(@#{@name})="#{@val}"|,
+        %|contains(normalize-space(@#{@name})," #{@val} ")|,
+        %|starts-with(normalize-space(@#{@name}),"#{@val} ")|,
+        %|ends-with(normalize-space(@#{@name})," #{@val}")|,
+      ].join(' or ')
+      @condition_should_equal = lambda do |config, expected|
+        @attr_matcher.new(@name, @val, XPF::Configuration.new(config)).
+          condition.should.equal(expected)
+      end
+    end
+
+    should 'have space normalized when config[:normalize_space] is true' do
+      @condition_should_equal[{:normalize_space => true}, @default]
+    end
+
+    should 'not have space normalized when config[:normalize_space] is false' do
+      expected = '(%s)' % [
+        %|%s="%s"|,
+        %|contains(%s," %s ")|,
+        %|starts-with(%s,"%s ")|,
+        %|ends-with(%s," %s")|,
+      ].join(' or ') % (["@#{@name}", @val]*4)
+      @condition_should_equal[{:normalize_space => false}, expected]
+    end
+
+    should 'be case-sensitive when config[:case_sensitive] is true' do
+      @condition_should_equal[{:case_sensitive => true}, @default]
+    end
+
+    should 'not be case-sensitive when config[:case_sensitive] is false' do
+      expected = '(%s)' % [
+        %|%s=#{translate_casing(%|"%s"|)}|,
+        %|contains(%s,#{translate_casing(%|" %s "|)})|,
+        %|starts-with(%s,#{translate_casing(%|"%s "|)})|,
+        %|ends-with(%s,#{translate_casing(%|" %s"|)})|,
+      ].join(' or ') % ([translate_casing("normalize-space(@#{@name})"), @val]*4)
+      @condition_should_equal[{:case_sensitive => false}, expected]
+    end
+
+  end
+
+  describe '> generating condition (w @class as a multi elements array)' do
+
+    before do
+      @name, @vals = :class, %w{aweso-me wonder-fu}
+      @default = @vals.map do |val|
+        '(%s)' % [
+          %|normalize-space(@#{@name})="#{val}"|,
+          %|contains(normalize-space(@#{@name})," #{val} ")|,
+          %|starts-with(normalize-space(@#{@name}),"#{val} ")|,
+          %|ends-with(normalize-space(@#{@name})," #{val}")|,
+        ].join(' or ')
+      end.join(' and ')
+      @condition_should_equal = lambda do |config, expected|
+        @attr_matcher.new(@name, @vals, XPF::Configuration.new(config)).
+          condition.should.equal(expected)
+      end
+    end
+
+    should 'have space normalized when config[:normalize_space] is true' do
+      @condition_should_equal[{:normalize_space => true}, @default]
+    end
+
+    should 'not have space normalized when config[:normalize_space] is false' do
+      expected = @vals.map do |val|
+        '(%s)' % [
+          %|@#{@name}="#{val}"|,
+          %|contains(@#{@name}," #{val} ")|,
+          %|starts-with(@#{@name},"#{val} ")|,
+          %|ends-with(@#{@name}," #{val}")|,
+        ].join(' or ')
+      end.join(' and ')
+      @condition_should_equal[{:normalize_space => false}, expected]
+    end
+
+    should 'be case-sensitive when config[:case_sensitive] is true' do
+      @condition_should_equal[{:case_sensitive => true}, @default]
+    end
+
+    should 'not be case-sensitive when config[:case_sensitive] is false' do
+      expected = @vals.map do |val|
+        '(%s)' % [
+          %|%s=#{translate_casing(%|"%s"|)}|,
+          %|contains(%s,#{translate_casing(%|" %s "|)})|,
+          %|starts-with(%s,#{translate_casing(%|"%s "|)})|,
+          %|ends-with(%s,#{translate_casing(%|" %s"|)})|,
+        ].join(' or ') % ([translate_casing("normalize-space(@#{@name})"), val]*4)
+      end.join(' and ')
+      @condition_should_equal[{:case_sensitive => false}, expected]
+    end
+
+  end
+
+end
+
+describe "XPF::HTML::Matchers::Attribute (w.r.t non @class)" do
+  before { @attr_matcher = XPF::HTML::Matchers::Attribute }
+  behaves_like 'basic attribute matcher'
 end
