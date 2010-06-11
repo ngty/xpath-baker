9ab00915fa2c813aaf5fc76f43d6ba3d856a6a9e
NgTzeYang <ngty77@gmail.com>
2010-06-11 15:16:57 +0800

    Default attribute matcher should be XPF::HTML::Matchers::Attribute.

--- lib/xpf/html.rb
+++ lib/xpf/html.rb
@@ -1,9 +1,14 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), '..', 'xpf')
 require 'xpf/html/tr'
+require 'xpf/html/matchers/attribute'
 
 module XPF
 
   declare_mode_as :html
+
+  configure do |config|
+    config.attribute_matcher = XPF::HTML::Matchers::Attribute
+  end
 
   extend HTML::Tr
 
