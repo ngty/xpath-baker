2191c27604197b235fdec3459c36e8003bc4cb98
NgTzeYang <ngty77@gmail.com>
2010-06-11 15:50:56 +0800

    Added helper #translate_casing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,9 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'xpf'
 
+def translate_casing(expr)
+  uc, lc = [('A'..'Z'), ('a'..'z')].map {|r| r.to_a.join('') }
+  %|translate(#{expr},"#{uc}","#{lc}")|
+end
+
 Bacon.summary_on_exit
