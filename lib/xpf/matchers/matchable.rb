1cb0713383908f08fb630f5909c82acc2d3d89af
NgTzeYang <ngty77@gmail.com>
2010-06-14 11:18:06 +0800

    The case of an axis appearing wo nodetest will never occur.

--- lib/xpf/matchers/matchable.rb
+++ lib/xpf/matchers/matchable.rb
@@ -19,7 +19,7 @@ module XPF
       end
 
       def mc(conditions)
-        (%w{self::* self}.include?(config.axis.to_s) && conditions.empty?) ? nil :
+        ('self::*' == config.axis && conditions.empty?) ? nil :
           p('./%s' % config.axis, conditions.empty? ? nil : ('[%s]' % conditions.join('][')))
       end
 
