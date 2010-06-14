f26d6e606218de3d82c84849d0c485791dfc1d7c
NgTzeYang <ngty77@gmail.com>
2010-06-14 15:23:54 +0800

    Useful helpers to cut away extra fat in fat.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,13 @@ def translate_casing(expr)
   %|translate(#{expr},"#{uc}","#{lc}")|
 end
 
+def each_xpf(&blk)
+  [XPF, send(:xpf)].each {|r| yield(r) }
+end
+
+def check_tokens(expr, tokens)
+  expr.extend(XPF::Matchers::Enhancements::String)
+  tokens.map{|token| expr.apply_check_for_token(token) }.join(' and ')
+end
+
 Bacon.summary_on_exit
