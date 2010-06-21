766391b4f530741b960310a181deebacb128c978
NgTzeYang <ngty77@gmail.com>
2010-06-21 08:13:03 +0800

    Updated specs to include array style configuration.

--- spec/xpf/matchers/group_spec.rb
+++ spec/xpf/matchers/group_spec.rb
@@ -12,59 +12,75 @@ describe "XPF::Matchers::Group" do
     end
 
     should "return nil if match attrs is empty & :axial_node is 'self::*'" do
-      @condition_should_equal[{}, {:axial_node => 'self::*'}, nil]
+      [{:axial_node => 'self::*'}, %w{self::*}].each do |config|
+        @condition_should_equal[{}, config, nil]
+      end
     end
 
     should "return nil if match attrs is empty & :axial_node is :self" do
-      @condition_should_equal[{}, {:axial_node => :self}, nil]
+      [{:axial_node => :self}, %w{self}].each do |config|
+        @condition_should_equal[{}, config, nil]
+      end
     end
 
     should "return axial_node expr if match attrs is empty & :axial_node is 'self::?' (with '?' not as '*')" do
-      @condition_should_equal[{}, {:axial_node => 'self::x'}, './self::x']
+      [{:axial_node => 'self::x'}, %w{self::x}].each do |config|
+        @condition_should_equal[{}, config, './self::x']
+      end
     end
 
     should 'return expr that reflect ONLY axial_node if specified match attrs is empty' do
-      @condition_should_equal[{}, {:axial_node => 'attribute::x'}, './attribute::x']
+      [{:axial_node => 'attribute::x'}, %w{attribute::x}].each do |config|
+        @condition_should_equal[{}, config, './attribute::x']
+      end
     end
 
     should "return expr that reflect prepending position if config[:position] is specified as '2^'" do
-      expected = './descendant::x[2][normalize-space(@attr1)]'
-      @condition_should_equal[[:attr1], {:axial_node => 'descendant::x', :position => '2^'}, expected]
+      [{:axial_node => 'descendant::x', :position => '2^'}, %w{descendant::x 2^}].each do |config|
+        @condition_should_equal[[:attr1], config, './descendant::x[2][normalize-space(@attr1)]']
+      end
     end
 
     should "return expr that reflect appending position if config[:position] is specified as '2'" do
-      expected = './descendant::x[normalize-space(@attr1)][2]'
-      @condition_should_equal[[:attr1], {:axial_node => 'descendant::x', :position => 2}, expected]
+      [{:axial_node => 'descendant::x', :position => 2}, %w{descendant::x 2}].each do |config|
+        @condition_should_equal[[:attr1], config, './descendant::x[normalize-space(@attr1)][2]']
+      end
     end
 
     should "return expr that reflect appending position if config[:position] is specified as '2$'" do
-      expected = './descendant::x[normalize-space(@attr1)][2]'
-      @condition_should_equal[[:attr1], {:axial_node => 'descendant::x', :position => '2$'}, expected]
+      [{:axial_node => 'descendant::x', :position => '2$'}, %w{descendant::x 2$}].each do |config|
+        @condition_should_equal[[:attr1], config, './descendant::x[normalize-space(@attr1)][2]']
+      end
     end
 
     should 'return expr that reflect text condition if match attrs is {:text => ..., ...}' do
-      expected = './descendant::*[normalize-space(.)="text-x"]'
-      @condition_should_equal[{:text => 'text-x'}, {:axial_node => 'descendant::*'}, expected]
+      [{:axial_node => 'descendant::*'}, %w{descendant::*}].each do |config|
+        @condition_should_equal[{:text => 'text-x'}, config, './descendant::*[normalize-space(.)="text-x"]']
+      end
     end
 
     should 'return expr that reflect presence of text if match attrs is [:text, ...]' do
-      expected = './descendant::*[normalize-space(.)]'
-      @condition_should_equal[[:text], {:axial_node => 'descendant::*'}, expected]
+      [{:axial_node => 'descendant::*'}, %w{descendant::*}].each do |config|
+        @condition_should_equal[[:text], config, './descendant::*[normalize-space(.)]']
+      end
     end
 
     should 'return expr that reflect attr condition if match attrs is {:attr => ..., ...}' do
-      expected = './descendant::*[normalize-space(@attr)="value-x"]'
-      @condition_should_equal[{:attr => 'value-x'}, {:axial_node => 'descendant::*'}, expected]
+      [{:axial_node => 'descendant::*'}, %w{descendant::*}].each do |config|
+        @condition_should_equal[{:attr => 'value-x'}, config, './descendant::*[normalize-space(@attr)="value-x"]']
+      end
     end
 
     should 'return expr that reflect presence of attr if match attrs is [:attr, ...]' do
-      expected = './descendant::*[normalize-space(@attr)]'
-      @condition_should_equal[[:attr], {:axial_node => 'descendant::*'}, expected]
+      [{:axial_node => 'descendant::*'}, %w{descendant::*}].each do |config|
+        @condition_should_equal[[:attr], config, './descendant::*[normalize-space(@attr)]']
+      end
     end
 
     should "return expr that reflect expr as literal if match attrs is ['position()=99', ...]" do
-      expected = './descendant::*[position()=99]'
-      @condition_should_equal[['position()=99'], {:axial_node => 'descendant::*'}, expected]
+      [{:axial_node => 'descendant::*'}, %w{descendant::*}].each do |config|
+        @condition_should_equal[['position()=99'], config, './descendant::*[position()=99]']
+      end
     end
 
   end
