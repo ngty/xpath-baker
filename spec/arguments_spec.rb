bf358bb8e59cb6b23168f4cb37b8df33c01f152d
NgTzeYang <ngty77@gmail.com>
2010-06-22 14:40:07 +0800

    Restructured specs to mimic lib directory structure.

--- spec/arguments_spec.rb
+++ spec/arguments_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+require File.join(File.dirname(__FILE__), 'spec_helper')
 require File.join(File.dirname(__FILE__), 'arguments_data')
 
 describe 'XPF::Arguments' do
