ddb726803ed0360aef446a256ffb2874071f1834
NgTzeYang <ngty77@gmail.com>
2010-06-22 14:40:49 +0800

    Should never have added this file.

--- spec/matchers/element.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', 'spec_helper')
-require File.join(File.dirname(__FILE__), 'basic_attribute_shared_spec')
-
-describe 'XPF::Matchers::Attribute' do
-  before { @attr_matcher = XPF::Matchers::Attribute }
-  behaves_like 'basic attribute matcher'
-end
